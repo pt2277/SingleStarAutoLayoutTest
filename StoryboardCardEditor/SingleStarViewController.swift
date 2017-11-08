f9b8cf3edc5dd51415e545ec20a5107c452cb50b
Papoj Thamjaroenporn <papojt@cs.columbia.edu>
2017-11-08 21:12:52 +0700

    Changed back to Storyboard version test.

--- StoryboardCardEditor/SingleStarViewController.swift
+++ StoryboardCardEditor/SingleStarViewController.swift
@@ -13,25 +13,25 @@ class SingleStarViewController: UIViewController {
     // Uncomment the code below to test the alternative for Size Class based constraint activation.
     // Be sure to delete / completely uninstall the width constraint in the storyboard first.
     
-    var starWidthConstraint: NSLayoutConstraint!
+//    var starWidthConstraint: NSLayoutConstraint!
     
     @IBOutlet weak var starImageVIew: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        starWidthConstraint = starImageVIew.widthAnchor.constraint(equalToConstant: 480)
-        starWidthConstraint.priority = .required
-        starWidthConstraint.isActive = true
+//        starWidthConstraint = starImageVIew.widthAnchor.constraint(equalToConstant: 480)
+//        starWidthConstraint.priority = .required
+//        starWidthConstraint.isActive = true
     }
     
-    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-        super.traitCollectionDidChange(previousTraitCollection)
-        if (traitCollection.horizontalSizeClass == .compact && traitCollection.verticalSizeClass == .regular) {
-            starWidthConstraint.isActive = false
-        } else {
-            starWidthConstraint.isActive = true
-        }
-    }
+//    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+//        super.traitCollectionDidChange(previousTraitCollection)
+//        if (traitCollection.horizontalSizeClass == .compact && traitCollection.verticalSizeClass == .regular) {
+//            starWidthConstraint.isActive = false
+//        } else {
+//            starWidthConstraint.isActive = true
+//        }
+//    }
 
 }
